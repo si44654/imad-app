4cd83a07958b8f13e9f26d09d58e6e8ddbaf6678
si44654 <31180880+si44654@users.noreply.github.com>
2017-08-23 17:07:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/article1',function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/article2',function(req, res){
-  res.send('article2 requested will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 app.get('/article3',function(req, res){
-  res.send('article3 requested will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
